09406d96ef8b2254e8d0c583355e6f73cb54e7af
Alam Garcia <garcia76@avaya.com>
2018-08-09 11:29:34 -0500

    Se agrega LocalStorage

--- js/custom-avaya.js
+++ js/custom-avaya.js
@@ -115,35 +115,64 @@ $(document).ready(function() {
     }
 
     $("#guardarbtn").click(function() {
-      $.ajax({
-             url: 'php/imagen.php',
-             type: 'POST',
-             data: new FormData(this),
-            mimeTypes:"multipart/form-data",
-            contentType: false,
-            cache: false,
-            processData: false, 
-             success: function (response) {
-               alert(response);
 
-               // console.log("Ajustes Guardados");
-               // var nombre = $('#snombre_txt').val();
-               // var poliza = $('#spoliza_txt').val();
-               // var host = $('#shost_txt').val();
-               // var numero = $('#snumero_txt').val();
-               // var puerto = $('#spuerto_txt').val();
-               // var empresa = $('#sempresa_txt').val();
-               // localStorage.setItem("nombre", nombre);
-               // localStorage.setItem("poliza", poliza);
-               // localStorage.setItem("host", host);
-               // localStorage.setItem("numero", numero);
-               // localStorage.setItem("puerto", puerto);
-               // localStorage.setItem("empresa", empresa);
-               // localStorage.setItem("ajustes", "1");
-               // $('#modalajustes').modal('hide');
-               // location.reload();
-             }
-         });
+               console.log("Ajustes Guardados");
+               var nombre = $('#snombre_txt').val();
+               var poliza = $('#spoliza_txt').val();
+               var host = $('#shost_txt').val();
+               var numero = $('#snumero_txt').val();
+               var puerto = $('#spuerto_txt').val();
+               var empresa = $('#sempresa_txt').val();
+               localStorage.setItem("nombre", nombre);
+               localStorage.setItem("poliza", poliza);
+               localStorage.setItem("host", host);
+               localStorage.setItem("numero", numero);
+               localStorage.setItem("puerto", puerto);
+               localStorage.setItem("empresa", empresa);
+               localStorage.setItem("ajustes", "1");
+               $('#modalajustes').modal('hide');
+                var bannerImage = document.getElementById('file');
+                var result = document.getElementById('res');
+                var img = document.getElementById('tableBanner');
+
+                //
+                bannerImage.addEventListener('change', function() {
+                    var file = this.files[0];
+                    if (file.type.indexOf('image') < 0) {
+                        res.innerHTML = 'invalid type';
+                        return;
+                    }
+                    var fReader = new FileReader();
+                    fReader.onload = function() {
+                        img.src = fReader.result;
+                        localStorage.setItem("imgData", getBase64Image(img));
+                    };
+
+                    fReader.readAsDataURL(file);
+                });
+
+                function getBase64Image(img) {
+                    var canvas = document.createElement("canvas");
+                    canvas.width = img.width;
+                    canvas.height = img.height;
+
+                    var ctx = canvas.getContext("2d");
+                    ctx.drawImage(img, 0, 0);
+
+                    var dataURL = canvas.toDataURL("image/png");
+
+                    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+                }
+
+                function fetchimage () {
+                    var dataImage = localStorage.getItem('imgData');
+                    img.src = "data:image/png;base64," + dataImage;
+                }
+
+                // Call fetch to get image from localStorage.
+                fetchimage();
+
+               location.reload();
 
 
     });
